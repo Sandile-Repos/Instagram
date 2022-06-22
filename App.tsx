3d0cb88349a92ec3b8cf8b998260f37965ef46b7
Sandile-Repos <sandile.developer@gmail.com>
2022-06-22 07:55:33 +0200

    add feed list from json file

--- App.tsx
+++ App.tsx
@@ -1,70 +1,15 @@
 import React from 'react';
 import {View, ScrollView, StyleSheet} from 'react-native';
 import FeedPost from './src/components/FeedPost';
-
-const post = {
-  id: '1',
-  createdAt: '16 June 2022',
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    username: 'sandiledladla',
-  },
-  nofComments: 11,
-  nofLikes: 33,
-  comments: [
-    {
-      id: '1',
-      comment: 'Hello there',
-      user: {
-        username: 'mandlandlovu',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        username: 'siphodladla',
-      },
-    },
-  ],
-};
-const post1 = {
-  id: '1',
-  createdAt: '16 June 2022',
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    username: 'sandiledladla',
-  },
-  nofComments: 11,
-  nofLikes: 33,
-  comments: [
-    {
-      id: '1',
-      comment: 'Hello there',
-      user: {
-        username: 'mandlandlovu',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        username: 'siphodladla',
-      },
-    },
-  ],
-};
+import posts from './src/assets/data/posts.json';
+import {IPost} from './src/types/models';
 
 const App = () => {
   return (
     <ScrollView style={styles.app}>
-      <FeedPost post={post} />
+      {posts.map((post: IPost) => (
+        <FeedPost key={post.id} post={post} />
+      ))}
     </ScrollView>
   );
 };
