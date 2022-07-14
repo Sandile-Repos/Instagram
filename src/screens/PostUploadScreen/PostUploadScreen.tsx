84c471c7389a05aaf2e30dcc2c2af82bc2a6334f
Sandile-Repos <sandile.developer@gmail.com>
2022-07-14 10:26:53 +0200

    add camera on post upload screen

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,14 +1,43 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Camera} from 'expo-camera';
 
 const PostUploadScreen = () => {
+  const [hasPermissions, setHasPermission] = useState<boolean | null>(null);
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      setHasPermission(
+        cameraPermission.status === 'granted' &&
+          microphonePermission.status === 'granted',
+      );
+    };
+    getPermission();
+  }, []);
+
+  if (hasPermissions === null) {
+    return <Text>Loading...</Text>;
+  }
+  if (hasPermissions === false) {
+    return <Text>No access to the camera</Text>;
+  }
   return (
-    <View>
-      <Text>Post Upload sScreen</Text>
-    </View>
+    <SafeAreaView style={styles.page}>
+      <Camera style={styles.camera} />
+    </SafeAreaView>
   );
 };
 
 export default PostUploadScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  camera: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+  },
+});
