22ef2f5383b87c4713d8bd844328429129990bba
Sandile-Repos <sandile.developer@gmail.com>
2022-07-18 08:27:20 +0200

    added stack navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import {View, Text, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import logo from '../assets/images/logo.png';
 
 import CommentsScreen from '../screens/CommentsScreen/CommentsScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
@@ -7,15 +12,36 @@ import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import PostUploadScreen from '../screens/PostUploadScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
+const Stack = createNativeStackNavigator();
+
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
-      {/* <CommentsScreen />
-      <ProfileScreen />
-      <EditProfileScreen />
-      <PostUploadScreen /> */}
+      <Stack.Navigator
+        initialRouteName="Feed"
+        screenOptions={{headerShown: true}}>
+        <Stack.Screen
+          name="Feed"
+          component={HomeScreen}
+          options={{headerTitle: HeaderTitle, headerTitleAlign: 'center'}}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={ProfileScreen}
+          options={{title: 'Profile'}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{width: 150, height: 40}}
+    />
   );
 };
 
