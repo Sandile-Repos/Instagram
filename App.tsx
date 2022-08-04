26f777c51d292bb5f6ebcec9ca78afc2a1787429
Sandile-Repos <sandile.developer@gmail.com>
2022-08-04 17:12:54 +0200

    add the default amplify signup,signin and signout

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 import Navigation from './src/navigation';
 import {Amplify} from 'aws-amplify';
 import config from './src/aws-exports';
+import {withAuthenticator} from 'aws-amplify-react-native';
 Amplify.configure(config);
 const App = () => {
   return (
@@ -12,7 +13,46 @@ const App = () => {
   );
 };
 
-export default App;
+// Add additional fields to the default auth flow
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      placeholder: 'Name',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+      placeholder: 'Email',
+    },
+    {
+      label: 'Username',
+      key: 'username',
+      required: true,
+      displayOrder: 3,
+      type: 'string',
+      placeholder: 'Username/handle',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 4,
+      type: 'password',
+      placeholder: 'Password',
+    },
+  ],
+};
+
+export default withAuthenticator(App, {signUpConfig, includeGreetings: true}); // includeGreetings default signout included in every screen
 
 const styles = StyleSheet.create({
   app: {
