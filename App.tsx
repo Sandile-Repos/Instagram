4c786c0df1081f6fc949677b64be6b9bfa392ba9
Sandile-Repos <sandile.developer@gmail.com>
2022-09-07 19:25:22 +0200

    add in app browser so you redirected on app rather than browser

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Linking} from 'react-native';
 import Navigation from './src/navigation';
 import {Amplify} from 'aws-amplify';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
+
 import config from './src/aws-exports';
 import AuthContextProvider from './src/contexts/AuthContext';
 
+const urlOpener = async (url: string, redirectUrl: string) => {
+  await InAppBrowser.isAvailable();
+  const response = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (response.type === 'success') {
+    Linking.openURL(response.url);
+  }
+};
+
 // Update specific fields on config if needed
-// const updatedConfig = {
-//   ...config,
-//   oauth: {
-//     ...config.oauth,
-//     redirectSignIn: 'instaphoto://',
-//     redirectSignOut: 'instaphoto://',
-//   },
-// };
-// Amplify.configure(updatedConfig);
-Amplify.configure(config);
+const updatedConfig = {
+  ...config,
+  oauth: {
+    ...config.oauth,
+    urlOpener,
+  },
+};
+Amplify.configure(updatedConfig);
 
 const App = () => {
   return (
