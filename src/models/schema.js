1634c74c6e0563aa8c36a8bc3d4b6aefbe650248
Sandile-Repos <Sandile-Repos@users.noreply.github.com>
2022-12-21 17:21:56 +0000

    Apply automatic changes

--- src/models/schema.js
+++ src/models/schema.js
@@ -570,6 +570,6 @@ export const schema = {
     },
     "enums": {},
     "nonModels": {},
-    "codegenVersion": "3.2.0",
+    "codegenVersion": "3.3.2",
     "version": "5b27c5a3fff6bb266d4d1c78c71b83cb"
 };
