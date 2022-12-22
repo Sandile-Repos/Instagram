094f1cc33b6d345c92fb72c88c36d96c9e6657ee
Sandile-Repos <Sandile-Repos@users.noreply.github.com>
2022-12-22 22:11:50 +0000

    Apply automatic changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,12 +3,12 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-2",
-    "aws_cognito_identity_pool_id": "us-east-2:15c98955-2c03-4dec-8738-c72dccfe04ff",
+    "aws_cognito_identity_pool_id": "us-east-2:2f476854-49ab-47e4-9781-d3c6e9c5ded4",
     "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": "us-east-2_3CBwEOXED",
-    "aws_user_pools_web_client_id": "kocvbb8hkcpjugnjjrdr1kot9",
+    "aws_user_pools_id": "us-east-2_7J2KugFK7",
+    "aws_user_pools_web_client_id": "1bu9ot3gpboju3t4o25cm2s5sd",
     "oauth": {
-        "domain": "ohhcpsxp8xxo-dev.auth.us-east-2.amazoncognito.com",
+        "domain": "ohhcpsxp8xxo-staging.auth.us-east-2.amazoncognito.com",
         "scope": [
             "phone",
             "email",
@@ -47,10 +47,10 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://crs572xx25g4tpfxgrbsmps7na.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://4lyborxrcfebjbolz6ektl7bmy.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_user_files_s3_bucket": "memories-album223658-dev",
+    "aws_user_files_s3_bucket": "memories-album203838-staging",
     "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
